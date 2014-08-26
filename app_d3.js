21a0855d0584d25329cdb3f47f62e8ae56fa2ffa
Dominic Czarnota <dczarno@gmail.com>
2014-08-25 19:30:24 -0700

    Fixed temp data set

--- app_d3.js
+++ app_d3.js
@@ -6,22 +6,49 @@ var myDataSet = {
   "Pierce the Veil": "1564"
 };
 
+var myDataSet = [
+  {
+    name: "The Beatles"
+    playcount: "2661"
+  },
+  {
+    name: "La Dispute"
+    playcount: "2437"
+  },
+  {
+    name: "Brand New"
+    playcount: "1932"
+  },
+  {
+    name: "Say Anything"
+    playcount: "1725"
+  },
+  {
+    name: "Pierce the Veil"
+    playcount: "1564"
+  },
+]
+
+
 //d3 append svg on username submission
 var svgAppend = function(){
-  d3.select('.d3-data').append('svg')
-    .attr('width', 800)
-    .attr('height', 500)
-    .style('border', '1px solid black')
+  var svg = d3.select('.d3-data').append('svg')
+            .attr('width', 800)
+            .attr('height', 500)
+            .style('border', '1px solid black');
 
-    .data(myDataSet)
-    .enter()
-    .append('div')
-    .append('circle')
-    .attr('cx', 25)
-    .attr('cy', 25)
-    .attr('r', 25)
-    .style('opacity', .5)
-    .style('fill', 'blue');
+  for(var key in myDataSet){
+    var circle = svg.append('circle');
+
+    // svg.selectAll('circle')
+    //     .data(myDataSet, function(myDataSet.d) { console.log(d); 
+    //       return d; })
+    //     .attr('cx', 25)
+    //     .attr('cy', 25)
+    //     .attr('r', 25)
+    //     .style('opacity', .5)
+    //     .style('fill', 'blue');
+  }
 };
 
 
