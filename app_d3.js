c5689a7ff04f8f5485675b654c86ec591d704fd3
Dominic Czarnota <dczarno@gmail.com>
2014-08-25 17:26:07 -0700

    Added temp data set

--- app_d3.js
+++ app_d3.js
@@ -1,7 +1,28 @@
+var myDataSet = {
+  "The Beatles": "2661",
+  "La Dispute": "2437",
+  "Brand New": "1932",
+  "Say Anything": "1725",
+  "Pierce the Veil": "1564"
+};
+
 //d3 append svg on username submission
 var svgAppend = function(){
   d3.select('.d3-data').append('svg')
     .attr('width', 800)
     .attr('height', 500)
-    .style('border', '1px solid black');
+    .style('border', '1px solid black')
+
+    .data(myDataSet)
+    .enter()
+    .append('div')
+    .append('circle')
+    .attr('cx', 25)
+    .attr('cy', 25)
+    .attr('r', 25)
+    .style('opacity', .5)
+    .style('fill', 'blue');
 };
+
+
+
