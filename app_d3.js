3565cb1126e9f3e80295c6bfb56a76dcad1f7364
Dominic Czarnota <dczarno@gmail.com>
2014-08-25 21:49:36 -0700

    Adds random x and y values for top artists

--- app_d3.js
+++ app_d3.js
@@ -1,42 +1,36 @@
-var myDataSet = [
-  {
-    name: "The Beatles",
-    playcount: "2661"
-  },
-  {
-    name: "La Dispute",
-    playcount: "2437"
-  },
-  {
-    name: "Brand New",
-    playcount: "1932"
-  },
-  {
-    name: "Say Anything",
-    playcount: "1725"
-  },
-  {
-    name: "Pierce the Veil",
-    playcount: "1564"
-  },
-]
-
+var svgSettings = {
+  width: 800,
+  height: 500
+};
 
 //d3 append svg on username submission
-var svgAppend = function(){
+var svgAppend = function(topArtistsArray){
+
+  var xRandom = function(){
+    var x = Math.random() * 800;
+    console.log("x: ",x);
+    return x;
+  };
+
+  var yRandom = function(){
+    var y = Math.random() * 500;
+    console.log("y: ",y);
+    return y;
+  };
+
   var svg = d3.select('.d3-data').append('svg')
-            .attr('width', 800)
-            .attr('height', 500)
+            .attr('width', svgSettings.width)
+            .attr('height', svgSettings.height)
             .style('border', '1px solid black');
   
   var circles = svg.selectAll("circle")
-               .data(myDataSet)
+               .data(topArtistsArray)
                .enter()
                .append("circle");
 
   var circleAttributes = circles
-                         .attr('cx', 25)
-                         .attr('cy', 25)
+                         .attr('cx', function(d){return xRandom();})
+                         .attr('cy', function(d){return yRandom();})
                          .attr('r', function(d){return (d.playcount/100);})
                          .style('opacity', .5)
                          .style('fill', 'blue');
